a934a88bf1e70c6f8e5d789736e2645a3192f1c3
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2022-12-27 14:28:25 +0500

    Git Init

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,54 @@
-console.log("TypeScript ATM Machine");
+import inquirer from "inquirer";
+import chalk from "chalk";
+import chalkAnimation from "chalk-animation";
+
+const runAnimation = () => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, 2000);
+    })
+};
+
+async function welcome() {
+    let animation = chalkAnimation.rainbow("Welcome to ATM Machine");
+    await runAnimation();
+
+    animation.stop();
+
+    myATM();
+};
+
+await welcome();
+
+async function myATM() {
+
+    type atmType = {
+        userId: string,
+        userPin: number
+    };
+
+    const atm: atmType = await inquirer.prompt([
+        {
+            name: "userId",
+            type: "input",
+            message: "Enter Your ID: "
+        },
+        {
+            name: "userPin",
+            type: "password",
+            message: "Enter Your PIN: "
+        },
+        {
+            name: "transactionType",
+            type: "list",
+            message: "Select Your Transaction: \n",
+            choices: [
+                "View Balance",
+                "Deposit",
+                "Withdrawal"
+            ]
+        }
+    ]);
+
+    console.log(chalk.cyan(`${atm.userId}`));
+
+};
